03b7fd738276544b6d44d6eefc3ed1f351d30db9
rvrita <rvrita@me.com>
2019-11-22 23:48:40 -0800

    Create problem_4.py

---
+++ problem_4.py
@@ -0,0 +1,67 @@
+class Group(object):
+    def __init__(self, _name):
+        self.name = _name
+        self.groups = []
+        self.users = []
+
+    def add_group(self, group):
+        self.groups.append(group)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def get_groups(self):
+        return self.groups
+
+    def get_users(self):
+        return self.users
+
+    def get_name(self):
+        return self.name
+
+
+def is_user_in_group(user, group):
+    """
+    Return True if user is in the group, False otherwise.
+
+    Args:
+      user(str): user name/id
+      group(class:Group): group to check user membership against
+    """
+    if type(group) != Group:
+        return False
+
+    if user in group.users:
+        return True
+
+    for subgroup in group.groups:
+        if is_user_in_group(user, subgroup) == True:
+            return True
+
+    return False
+
+# Test Cases
+parent = Group("parent")
+child = Group("child")
+sub_child = Group("subchild")
+sub_child_without_user = Group("subchild without user")
+sub_child_user = "sub_child_user"
+sub_child.add_user(sub_child_user)
+child.add_group(sub_child)
+child.add_group(sub_child_without_user)
+parent.add_group(child)
+
+print(is_user_in_group("sub_child_user", sub_child))
+# True
+
+print(is_user_in_group("nonexistent_user", sub_child))
+# False
+
+print(is_user_in_group("sub_child_user", parent))
+# True
+ 
+print(is_user_in_group("sub_child_user", sub_child_without_user))
+# False
+
+print(is_user_in_group(None, None)) 
+# False
