a7674fdee5ee7a1ccef5212bdb5ba1bc99a31401
rvrita <rvrita@me.com>
2019-12-01 16:18:26 -0800

    Switch huffman encoding to use built-in heapq

--- problem_3.py
+++ problem_3.py
@@ -1,4 +1,5 @@
 import sys
+from heapq import heappush, heappop
 from collections import defaultdict
 from pprint import pprint
 
@@ -11,48 +12,8 @@ class BinaryNode(object):
     def __repr__(self):
         return "Node" + repr(vars(self))
 
-# Let's implement a heap from scratch
-class Heap(object):
-    def __init__(self):
-        self.arr = []
-        self.size = 0
-
-    def insert(self, tuple):
-        # tuple[0] will be used for value comparison
-        self.arr.append(tuple)
-        self.bubble_up(self.size)
-        self.size += 1
-
-    def delete(self):
-        # swap first and last values in heap
-        self.arr[0], self.arr[-1] = self.arr[-1], self.arr[0]
-        val = self.arr.pop(-1)
-        self.size -= 1
-        self.sift_down(0)
-        return val
-
-    def bubble_up(self, i):
-        parent = int((i - 1) / 2);
-        if (self.arr[parent]):
-            if (self.arr[i][0] < self.arr[parent][0]):
-                # swap
-                self.arr[i], self.arr[parent] = self.arr[parent], self.arr[i]
-                self.bubble_up(parent)
-
-    def sift_down(self, i):
-        left = i * 2 + 1
-        right = i * 2 + 2
-        largest = i
-        arr = self.arr
-        size = self.size
-
-        if left < size and arr[left][0] < arr[largest][0]:
-            largest = left
-        if right < size and arr[right][0] < arr[largest][0]:
-            largest = right
-        if largest != i:
-            arr[i], arr[largest] = arr[largest], arr[i]
-            self.sift_down(largest)
+    def __lt__(self, other):
+        return True
 
 def walk_tree(node, path):
     if not node:
@@ -67,51 +28,20 @@ def huffman_encoding(data):
     for char in data:
         char2freq[char] += 1 # without defaultdict: ... = char2freq.get(char, 0) + 1
 
-    # Method One: implicit Huffman tree from heap
-    # heap = Heap()
-    # for char, freq in char2freq.items():
-    #     # freq will be used as value for comparison
-    #     heap.insert([freq, [char, ""]])
-
-    # pprint(heap.arr)
-
-    # while heap.size > 1:
-    #     lo = heap.delete()
-    #     hi = heap.delete()
-    #     print(lo, hi)
-    #     for pair in lo[1:]:
-    #         pair[1] = '0' + pair[1]
-    #     for pair in hi[1:]:
-    #         pair[1] = '1' + pair[1]
-    #     heap.insert([lo[0] + hi[0]] + lo[1:] + hi[1:])
-
-    # pprint(heap.arr)
-
-    # lookup = {}
-    # pairs = heap.delete()[1:]
-    # for char, binary in pairs:
-    #     lookup[char] = binary
-
-    # encoded_data = ""
-    # for char in data:
-    #     encoded_data += lookup[char]
-
-    # pprint(lookup)
-    # print(encoded_data)
-
-    # Method Two: build explicit Huffman tree
-    pq = Heap() # priority queue
+    # Build Huffman tree
+    pq = [] # priority queue
     for char, freq in char2freq.items():
-        pq.insert([freq, BinaryNode(char)])
+        # Heap operations use the first item (freq) for value comparison
+        heappush(pq, [freq, BinaryNode(char)])
 
-    while pq.size > 1:
-        left = pq.delete()
-        right = pq.delete()
+    while len(pq) > 1:
+        left = heappop(pq)
+        right = heappop(pq)
         freq = left[0] + right[0]
         parent = BinaryNode(None, left[1], right[1])
-        pq.insert([freq, parent])
+        heappush(pq, [freq, parent])
 
-    tree = pq.delete()[1]
+    tree = heappop(pq)[1]
 
     # Build dictionary
     lookup = walk_tree(tree, '')
