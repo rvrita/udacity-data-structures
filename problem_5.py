cd178bdaf8f2a43fb143b71fb1eb98f38c059e92
Rita Rózsavölgyi <rvrita@me.com>
2019-12-04 00:55:46 -0800

    Update problem_5.py

--- problem_5.py
+++ problem_5.py
@@ -1,28 +1,105 @@
 import hashlib
+import datetime
 
 class Block:
-    def __init__(self, timestamp, data, prev_block):
+    def __init__(self, timestamp, data, previous_hash):
         self.ts = timestamp
         self.data = data
-        self.prev_block = prev_block
+        self.previous_hash = previous_hash
         self.hash = self.calc_hash()
 
     def __str__(self):
-        return "[ {} {} ]-> ".format(self.data[:3], self.hash[:3]) + str(self.prev_block)
+        return ("timestamp: {}\n" 
+            "data: {}\n"
+            "hash: {}\n"
+            "previous_hash: {}") \
+        .format(self.ts, self.data, self.hash, self.previous_hash)
 
     def calc_hash(self):
         sha = hashlib.sha256()
-        prev_hash = self.prev_block.hash if self.prev_block else 0
-        hash_str = "{} {} {}".format(prev_hash, self.ts, self.data).encode('utf-8')
+        hash_str = "{} {} {}".format(self.previous_hash, self.ts, self.data).encode('utf-8')
         sha.update(hash_str)
         return sha.hexdigest()
 
-# Test Data
-blockchain_head = None
-blockchain_head = Block(10000, "Hey", blockchain_head)
-blockchain_head = Block(20000, "Foo", blockchain_head)
-blockchain_head = Block(20000, "Foo", blockchain_head)
-blockchain_head = Block(30000, "Bar", blockchain_head)
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
-# [ Bar 325 ]-> [ Foo 1b0 ]-> [ Foo 9a1 ]-> [ Hey 7d2 ]-> None
-print(blockchain_head)
+    def __str__(self):
+        output = "{}\n----------------\n".format(self.value)
+        if self.next:
+            return output + str(self.next) 
+        else:
+            return output
+
+        return output + str(self.next) if self.next else output
+
+        return self.next ? output + str(self.next) : output
+
+class Blockchain:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __str__(self):
+        if self.head:
+            return str(self.head) 
+        else:
+            return "Empty blockchain!\n"
+
+    def add_block(self, data):
+        timestamp = datetime.datetime.now()
+        head = self.head
+        previous_hash = head.value.hash if head is not None else None;
+        block = Block(timestamp, data, previous_hash)
+        node = Node(block)
+        if self.tail:
+            self.tail.next = node
+        if self.head is None:
+            self.head = node
+        self.tail = node
+
+
+# Test Data
+
+blockchain = Blockchain()
+blockchain.add_block("Block 1")
+blockchain.add_block("Block 2")
+blockchain.add_block("Block 3")
+print(blockchain)
+# timestamp: 2019-12-04 00:44:21.641376
+# data: Block 1
+# hash: 06ce343e4c14e1f24ec1aa544cc40559cdcb4e306285cb91ac4ac77949eedc8f
+# previous_hash: None
+# ----------------
+# timestamp: 2019-12-04 00:44:21.641413
+# data: Block 2
+# hash: 4a7330a16d02cae9e91dffbb28898d90c3f96c4aee384aaf848f728a3b7bd891
+# previous_hash: 06ce343e4c14e1f24ec1aa544cc40559cdcb4e306285cb91ac4ac77949eedc8f
+# ----------------
+# timestamp: 2019-12-04 00:44:21.641422
+# data: Block 3
+# hash: 2fd8e93754b26d4ceba701b93c0a321ff6aa4c26c894c94d11674dc59f2601cf
+# previous_hash: 06ce343e4c14e1f24ec1aa544cc40559cdcb4e306285cb91ac4ac77949eedc8f
+# ----------------
+
+blockchain_with_none_data = Blockchain()
+blockchain_with_none_data.add_block(None)
+blockchain_with_none_data.add_block("")
+print(blockchain_with_none_data)
+# timestamp: 2019-12-04 00:44:21.641479
+# data: None
+# hash: 0d66c9485ab19f7b9c071db0e475c5495c5bd90354fd31c1cec550d4d85dc95e
+# previous_hash: None
+# ----------------
+# timestamp: 2019-12-04 00:44:21.641506
+# data:
+# hash: 82baea3aeb5dc8fb9c0491ae932427c7211991b95dc56a63481b62ec0de5b240
+# previous_hash: 0d66c9485ab19f7b9c071db0e475c5495c5bd90354fd31c1cec550d4d85dc95e
+# ----------------
+
+
+empty_blockchain = Blockchain() 
+print(empty_blockchain)
+# Empty blockchain!
