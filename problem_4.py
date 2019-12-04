fb0caa6aa6092aa901027f0b44318c4130dda7a3
Rita Rózsavölgyi <rvrita@me.com>
2019-12-04 01:23:08 -0800

    Update problem_4.py
    minor change, separate test case 4

--- problem_4.py
+++ problem_4.py
@@ -1,67 +1,120 @@
-class Group(object):
-    def __init__(self, _name):
-        self.name = _name
-        self.groups = []
-        self.users = []
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
-    def add_group(self, group):
-        self.groups.append(group)
-
-    def add_user(self, user):
-        self.users.append(user)
-
-    def get_groups(self):
-        return self.groups
-
-    def get_users(self):
-        return self.users
-
-    def get_name(self):
-        return self.name
+    def __str__(self):
+        return "{} -> ".format(self.value) + str(self.next)
 
 
-def is_user_in_group(user, group):
-    """
-    Return True if user is in the group, False otherwise.
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-    Args:
-      user(str): user name/id
-      group(class:Group): group to check user membership against
-    """
-    if type(group) != Group:
-        return False
+    def __str__(self):
+        return str(self.head)
 
-    if user in group.users:
-        return True
+    def append(self, value):
 
-    for subgroup in group.groups:
-        if is_user_in_group(user, subgroup) == True:
-            return True
+        if self.head is None:
+            self.head = Node(value)
+            return
 
-    return False
+        node = self.head
+        while node.next:
+            node = node.next
 
-# Test Cases
-parent = Group("parent")
-child = Group("child")
-sub_child = Group("subchild")
-sub_child_without_user = Group("subchild without user")
-sub_child_user = "sub_child_user"
-sub_child.add_user(sub_child_user)
-child.add_group(sub_child)
-child.add_group(sub_child_without_user)
-parent.add_group(child)
+        node.next = Node(value)
 
-print(is_user_in_group("sub_child_user", sub_child))
-# True
+    def size(self):
+        size = 0
+        node = self.head
+        while node:
+            size += 1
+            node = node.next
 
-print(is_user_in_group("nonexistent_user", sub_child))
-# False
+        return size
 
-print(is_user_in_group("sub_child_user", parent))
-# True
- 
-print(is_user_in_group("sub_child_user", sub_child_without_user))
-# False
 
-print(is_user_in_group(None, None)) 
-# False
+def union(llist_1, llist_2):
+    # set uses a hash map, which will give better than O(n) insertion time
+    values = set()
+    for node in [llist_1.head, llist_2.head]:
+        while node:
+            values.add(node.value)
+            node = node.next
+    union = LinkedList()
+    for value in values:
+        union.append(value)
+    return union
+
+
+def intersection(llist_1, llist_2):
+    set1 = set()
+    node = llist_1.head
+    while node:
+        set1.add(node.value)
+        node = node.next
+
+    set2 = set()
+    node = llist_2.head
+    while node:
+        if node.value in set1:
+            set2.add(node.value)
+        node = node.next
+
+    intersection = LinkedList()
+    for value in set2:
+        intersection.append(value)
+    return intersection
+
+
+# Test case 1
+
+linked_list_1 = LinkedList()
+linked_list_2 = LinkedList()
+
+elements_1 = [3,2,4,35,6,65,6,4,3,21]
+elements_2 = [6,32,4,9,6,1,11,21,1]
+
+for i in elements_1:
+    linked_list_1.append(i)
+
+for i in elements_2:
+    linked_list_2.append(i)
+
+# 32 -> 65 -> 2 -> 35 -> 3 -> 4 -> 6 -> 1 -> 9 -> 11 -> 21 -> None
+print (union(linked_list_1,linked_list_2))
+# 4 -> 21 -> 6 -> None
+print (intersection(linked_list_1,linked_list_2))
+
+# Test case 2
+
+linked_list_3 = LinkedList()
+linked_list_4 = LinkedList()
+
+elements_3 = [3,2,4,35,6,65,6,4,3,23]
+elements_4 = [1,7,8,9,11,21,1]
+
+for i in elements_3:
+    linked_list_3.append(i)
+
+for i in elements_4:
+    linked_list_4.append(i)
+
+# 65 -> 2 -> 35 -> 3 -> 4 -> 6 -> 1 -> 7 -> 8 -> 9 -> 11 -> 21 -> 23 -> None
+print (union(linked_list_3,linked_list_4))
+# None
+print (intersection(linked_list_3,linked_list_4))
+
+# Test case 3 - union of two empty lists
+
+linked_list_5 = LinkedList()
+
+# None
+print(union(linked_list_5, linked_list_5))
+
+# Test case 4 -  intersection of same list
+
+# 1 -> 7 -> 8 -> 9 -> 11 -> 21 -> None
+print(intersection(linked_list_4, linked_list_4))
